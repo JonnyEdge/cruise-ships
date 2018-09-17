efdeb3143b2cd3b29d587121c5a2180d3c382287
Jonny Edge <jonnyedge@outlook.com>
2018-09-17 20:55:29 +0100

    Add function to Ship object, enabling it to setSail from a starting port to a designated destination.

--- __tests__/ships.test.js
+++ __tests__/ships.test.js
@@ -12,3 +12,15 @@ test('Adds a passenger property to the Ship object', () => {
   ship = new Ship();
   expect(ship.passengers).toEqual();
 });
+
+test('allows Ship to set sail from a defined starting port', () => {
+  ship = new Ship()
+  ship.setSail('Liverpool', 'Dover');
+  expect(ship.startPort).toEqual('Liverpool');
+});
+
+test('allows Ship to set sail to a defined destination port', () => {
+  ship = new Ship()
+  ship.setSail('Liverpool', 'Dover');
+  expect(ship.currentPort).toEqual('Dover');
+});
--- src/ships.js
+++ src/ships.js
@@ -1,5 +1,12 @@
 function Ship(passengers) {
-  this.passengers = passengers
+  this.passengers = passengers;
+  this.startPort = '';
+  this.currentPort = '';
+};
+
+Ship.prototype.setSail = function setSail(start, destination) {
+  this.startPort = start;
+  this.currentPort = destination;
 };
 
 // export default Ship;
