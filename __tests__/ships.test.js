f35408f05e5cdc6f9f4a8501a692491c8510bc26
JonnyEdge <jonnyedge@outlook.com>
2018-09-23 13:18:18 +0100

    Starting from the beginning using the code from my walkthrough run when necessary, but not relying on the walkthrough content itself. Created Ship object, and passing a named startPort string to its one and only startPort property.

--- __tests__/ships.test.js
+++ __tests__/ships.test.js
@@ -1,80 +1,15 @@
-const {
-  Ship,
-  Port,
-  Itinerary,
-} = require('../src/ships');
+const Ship = require ('../src/ships');
 
-let ship = null;
-let port = null;
-let itinerary = null;
+describe('Ship', () => {
+  it('Creates a Ship object', () => {
+    ship = new Ship();
 
-test('A Ship object is created', () => {
-  ship = new Ship();
-  expect(ship).toBeInstanceOf(Object);
+    expect(ship).toBeInstanceOf(Object);
+  });
+
+  it('Has a startPort property', () => {
+    ship = new Ship('Dover');
+
+    expect(ship.startPort).toEqual('Dover')
+  })
 });
-
-test('A Port object is created', () => {
-  port = new Port();
-  expect(port).toBeInstanceOf(Object);
-});
-
-test('An Itinerary object is created', () => {
-  itinerary = new Itinerary();
-  expect(itinerary).toBeInstanceOf(Object);
-});
-
-xtest('Adds a passenger property to the Ship object', () => {
-  ship = new Ship();
-  expect(ship.passengers).toEqual();
-});
-
-xtest('Adds a ports property to the Itinerary object', () => {
-  itinerary = new Itinerary();
-  expect(itinerary.ports).toEqual('');
-});
-
-test('Passes a name to the Port object name property', () => {
-  port = new Port('Liverpool');
-  expect(port.name).toEqual('Liverpool');
-});
-
-test('Allows Ship to set sail from a defined starting port', () => {
-  ship = new Ship()
-  ship.setSail('Liverpool', 'Dover');
-  expect(ship.startPort).toEqual('Liverpool');
-});
-
-test('Allows Ship to set sail to a defined destination port', () => {
-  ship = new Ship()
-  ship.setSail('Liverpool', 'Dover');
-  expect(ship.currentPort).toEqual('Dover');
-});
-
-test('Creates a new Port object and assigns to the startPort property of Ship object', () => {
-  ship = new Ship('', 'Liverpool')
-  expect(ship.startPort).toEqual('Liverpool');
-});
-
-test('Creates two new Port objects, one for the startPort, one for the docked port', () => {
-  ship = new Ship('', 'Liverpool')
-  ship.dock('Dover')
-  expect(ship.currentPort.name).toEqual('Dover');
-});
-
-test('Running setSail after dock function populates the previousPort property', () => {
-  ship = new Ship('', 'Liverpool')
-  ship.dock('Liverpool')
-  ship.setSail(ship.currentPort, 'Dover')
-  expect(ship.previousPort.name).toEqual('Liverpool');
-});
-
-test('Running setSail without dock function does not populate the previousPort property', () => {
-  ship = new Ship('', 'Liverpool')
-  ship.setSail(ship.currentPort, 'Dover')
-  expect(ship.previousPort).toEqual('Liverpool');
-});
-
-test('Using new Itinerary object to populate itinerary property of Ship object with array', () => {
-  ship = new Ship('', 'Liverpool', 'Dover');
-  expect(ship.itinerary.ports).toBeInstanceOf(Array);
-})
--- src/ships.js
+++ src/ships.js
@@ -1,31 +1,5 @@
-function Port(name) {
-  this.name = name;
-}
-
-function Itinerary(portName1, portName2) {
-  this.ports = [new Port(portName1), new Port(portName2)];
-}
-
-function Ship(passengers, portName1, portName2) {
-  this.passengers = passengers;
-  this.itinerary = new Itinerary(portName1, portName2);
-  this.startPort = this.itinerary[0];
-  this.previousPort = null;
-  this.currentPort = this.itinerary[1];
+function Ship(startPort) {
+  this.startPort = startPort
 };
 
-Ship.prototype.setSail = function setSail(start, destination) {
-  this.previousPort = this.currentPort;
-  this.startPort = start;
-  this.currentPort = destination;
-};
-
-Ship.prototype.dock = function dock(name) {
-  this.currentPort = this.itinerary[1];
-}
-
-module.exports = {
-  Ship,
-  Port,
-  Itinerary,
-};
+module.exports = Ship;
