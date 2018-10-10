066ab46fa47f68f7ac795c1aef932a5714fe3cb6
Jonny Edge <jonnyedge@outlook.com>
2018-10-10 20:34:09 +0100

    Created HTML and CSS files to start creating a GUI for the JS already created. A blank page at this stage with a pixel art water graphic that alternates between two separate images to animate it.

---
+++ src/controller.js
@@ -0,0 +1,15 @@
+function Controller() {
+  this.initialiseSea();
+}
+
+Controller.prototype.initialiseSea = function initialiseSea() {
+  const backgrounds = [
+    './images/water0.png',
+    './images/water1.png',
+  ];
+  let backgroundIndex = 0;
+  window.setInterval(() => {
+    document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+    backgroundIndex += 1;
+  }, 1000);
+};
